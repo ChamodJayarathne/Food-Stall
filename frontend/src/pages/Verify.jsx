1e6d680a45c0a17587664cd7a5033ef8ae62f550
ChamodJayarathne <148670748+ChamodJayarathne@users.noreply.github.com>
2024-10-02 17:06:25 +0530

    complete order without order status

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,10 +1,41 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import { useContext } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
 
 const Verify = () => {
-  return (
-    <div>Verify</div>
-  )
-}
+  const [searchParms, setSearchParams] = useSearchParams();
+  const success = searchParms.get("success");
+  const orderId = searchParms.get("orderId");
+  const navigate = useNavigate();
+  const { url } = useContext(ShopContext);
 
-export default Verify
+  
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", {
+      success,
+      orderId,
+    });
+    if (response.data.success) {
+      navigate("/myorders");
+    } else {
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, []);
+
+  return (
+    <div>
+      <div className="min-h-[60vh] grid">
+        <div className="h-24 w-24 place-self-center border-4 border-t-secondary rounded-full animate-spin"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Verify;
